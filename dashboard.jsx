62823d230537eb9684d0ab8b2d15288e5dc931a3
leoliu123456 <leo---liu@hotmail.com>
2016-09-28 00:01:38 +0800

    Add files via upload

---
+++ dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Dashboard extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="row">
+                    <div className="columns large-12"><div>Dashboard</div></div>
+                </div>
+            </div>
+        );
+    }
+}
